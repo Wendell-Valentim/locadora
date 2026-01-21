0d476b9bba5a83b7d2a72fe9bf91d197fc1bcb22
Wendell-Valentim <wendellldasiilva8@gmail.com>
2026-01-21 14:11:38 -0300

    test: implement unit and functional tests for Carro CRUD

--- src/main/java/io/github/wendellvalentim/locadora/controller/CarroController.java
+++ src/main/java/io/github/wendellvalentim/locadora/controller/CarroController.java
@@ -1,15 +1,15 @@
 package io.github.wendellvalentim.locadora.controller;
 
 import io.github.wendellvalentim.locadora.entity.CarroEntity;
+import io.github.wendellvalentim.locadora.exceptions.EntityNotFoundException;
 import io.github.wendellvalentim.locadora.service.CarroService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/carros")
@@ -30,4 +30,39 @@ public class CarroController {
         }
 
         }
+
+
+     @GetMapping("{id}")
+        public ResponseEntity<CarroEntity> buscar(@PathVariable("id") Long id) {
+        try {
+            var carro = carroService.buscarPorId(id);
+            return ResponseEntity.ok(carro);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+     }
+
+     @PutMapping("{id}")
+    public ResponseEntity<Void> atualizarCarro(@PathVariable("id") Long id,@RequestBody
+     CarroEntity carro) {
+        try{
+        carroService.atualizar(id, carro);
+        return ResponseEntity.noContent().build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+
+     }
+
+     @DeleteMapping("{id}")
+    public ResponseEntity<Void> atualizarCarro(@PathVariable("id") Long id) {
+        try{
+            carroService.deletar(id);
+            return ResponseEntity.noContent().build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
 }
--- src/test/java/io/github/wendellvalentim/locadora/controller/CarroControllerTest.java
+++ src/test/java/io/github/wendellvalentim/locadora/controller/CarroControllerTest.java
@@ -1,6 +1,7 @@
 package io.github.wendellvalentim.locadora.controller;
 
 import io.github.wendellvalentim.locadora.entity.CarroEntity;
+import io.github.wendellvalentim.locadora.exceptions.EntityNotFoundException;
 import io.github.wendellvalentim.locadora.service.CarroService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -11,14 +12,10 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(CarroController.class)
 public class CarroControllerTest {
@@ -40,7 +37,7 @@ public class CarroControllerTest {
         String json = """
                 {
                  "modelo": "Honda Civic",
-                 "valorDiaria": 150,
+                 "valorDiaria": 150.0,
                  "ano": 2027
                 }
                 """;
@@ -57,5 +54,79 @@ public class CarroControllerTest {
                 .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.modelo").value("Honda Civic"));
 
+    }
+
+    @Test
+    void deveObterDetalhesCarro() throws Exception{
+        when(carroService.buscarPorId(Mockito.any())).
+                thenReturn(new CarroEntity(1L, "Civic", 250, 2028));
+        mvc.perform(
+                MockMvcRequestBuilders.get("/carros/1")
+        ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.modelo").value("Civic"))
+                .andExpect(jsonPath("$.valorDiaria").value(250))
+                .andExpect(jsonPath("$.ano").value(2028));
+
+    }
+
+    @Test
+    void deveRetornarNotFoundAoTentarObterDetalhesCarro() throws Exception{
+        when(carroService.buscarPorId(Mockito.any())).thenThrow(EntityNotFoundException.class);
+        mvc.perform(
+                        MockMvcRequestBuilders.get("/carros/1")
+                ).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void deveAtualizarUmCarro() throws Exception{
+        when(carroService.atualizar(Mockito.any(), Mockito.any())).
+                thenReturn(new CarroEntity(1L, "Celta", 100,2025));
+        String json = """
+                {
+                 "modelo": "Celta",
+                 "valorDiaria": 100,
+                 "ano": 2025
+                }    
+                """;
+
+
+        mvc.perform(MockMvcRequestBuilders.put("/carros/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)).andExpect(status().isNoContent());
+    }
+
+    @Test
+    void deveRetornarNotFoundAoTentarAtualizarCarroInexistente() throws Exception{
+        when(carroService.atualizar(Mockito.any(),Mockito.any())).thenThrow(EntityNotFoundException.class);
+        String json = """
+                {
+                 "modelo": "Celta",
+                 "valorDiaria": 100,
+                 "ano": 2025
+                }    
+                """;
+        mvc.perform(
+                MockMvcRequestBuilders.put("/carros/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+        ).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void deveDeletarUmcarro() throws Exception {
+        doNothing().when(carroService).deletar(Mockito.any());
+
+        mvc.perform(MockMvcRequestBuilders.delete("/carros/1"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void deveRetornarNotFoundAoTentarDeletarCarroInexistente() throws Exception{
+        doThrow(EntityNotFoundException.class).when(carroService).deletar(Mockito.any());
+
+        mvc.perform(
+                MockMvcRequestBuilders.delete("/carros/1")
+        ).andExpect(status().isNotFound());
     }
 }
